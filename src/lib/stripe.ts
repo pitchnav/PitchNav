d9a29b6c746df23b980e9dac15bb3a37b7c56ced
Luke Condrin <lukecondrin@gmail.com>
2026-07-14 15:08:49 -0400

    Fix Stripe API version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 // Singleton server-side Stripe client
 // Only import from Server Components or Route Handlers
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 })
 
